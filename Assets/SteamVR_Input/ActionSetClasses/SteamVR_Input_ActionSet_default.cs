c3da8ff0028c216b64f87613c094b63d001c8906
Michael Claudio <michael.a.claudio@gmail.com>
2019-07-30 01:28:54 -0400

    Add Swap Action and Component
    It's something!

--- Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
+++ Assets/SteamVR_Input/ActionSetClasses/SteamVR_Input_ActionSet_default.cs
@@ -89,6 +89,14 @@ namespace Valve.VR
             }
         }
         
+        public virtual SteamVR_Action_Boolean Swap
+        {
+            get
+            {
+                return SteamVR_Actions.default_Swap;
+            }
+        }
+        
         public virtual SteamVR_Action_Vibration Haptic
         {
             get
---
+++ Assets/Swap.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+public class Swap : MonoBehaviour
+{
+    public SteamVR_Input_Sources handType; // 1
+    public SteamVR_Action_Boolean grabGripAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabGrip");
+    public SteamVR_Action_Boolean swapAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Swap");
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetGrab())
+        {
+            print("Grab " + handType);
+        }
+
+        if(GetSwap())
+        {
+            print("Swap " + handType);
+        }
+    }
+
+    public bool GetGrab() // 2
+    {
+        return grabGripAction.GetStateDown(handType);
+    }
+
+    public bool GetSwap()
+    {
+        return swapAction.GetStateDown(handType);
+    }
+}
